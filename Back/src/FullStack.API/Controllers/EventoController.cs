accea3c0c1360b1d2861a6ec232c3963b4309ab2
DeltaTecnologiaTI <101055517+DeltaTecnololiaTI@users.noreply.github.com>
2022-03-16 10:43:17 -0300

    Primeiro Commit

---
+++ Back/src/FullStack.API/Controllers/EventoController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FullStack.API.Data;
+using FullStack.API.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace FullStack.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        //Fazer referencia do context do BD dentro da controller
+        private readonly DataContext contexto;
+
+        public EventoController(DataContext contexto)
+        {
+            this.contexto = contexto;
+
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return contexto.Eventos;
+        }
+        /// <summary>
+        /// Busca elementos por Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public Evento GetById(int id)
+        {
+            return contexto.Eventos.FirstOrDefault(evento => evento.EventoId == id);
+
+        }
+        [HttpPost]
+        public string Post()
+        {
+            return "Teste do Evento POST";
+        }
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+            return $"Teste do Evento Put {id}";
+        }
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return $"Teste do Evento Delete {id}";
+        }
+    }
+}
---
+++ Back/src/FullStack.API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using FullStack.API.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullStack.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base(options)
+        {
+            
+        }
+        public DbSet<Evento> Eventos{get; set;}
+    }
+}
